a01ae241dcf69fa5c2a8fb30930abd0b8324bc67
dixonshen <dixonshen@hotmail.com>
2016-04-05 15:01:44 +0800

    Scan one time

--- src/WaterCapacity.java
+++ src/WaterCapacity.java
@@ -1,28 +1,35 @@
 /**
  * Created by DixonShen on 2016/4/1.
+ * Twitterm面试题之装水问题
  */
 public class WaterCapacity {
 
     public static void main(String[] args) {
-        int[] wall = {2,5,1,3,1,2,1,7,7,6};
+        int[] wall = {6, 1, 4, 6, 7, 5, 1, 6, 4};
         calculate(wall);
 
     }
 
     public static void calculate(int[] a){
         int volume = 0;
-        int lamx = 0;
-        int rmax = 0;
-        int i = 0,j = a.length-1;
-        while(i!=j){
-            if(a[i]>=lamx) lamx = a[i];
-            else volume += lamx-a[i];
-            i++;
-            if(i==j) break;
-            if(a[j]>=rmax) rmax = a[j];
-            else volume += rmax-a[j];
-            j--;
-            if(i==j) break;
+        int lp = 0,rp = a.length-1;
+        int lmax = a[lp];
+        int rmax = a[rp];
+        while(lp<rp){
+            if (lmax<rmax){
+                lp++;
+                if (a[lp]>=lmax)
+                    lmax = a[lp];
+                else
+                    volume += lmax-a[lp];
+            }
+            else {
+                rp--;
+                if (a[rp]>=rmax)
+                    rmax = a[rp];
+                else
+                    volume += rmax-a[rp];
+            }
         }
 
         System.out.println(volume);
